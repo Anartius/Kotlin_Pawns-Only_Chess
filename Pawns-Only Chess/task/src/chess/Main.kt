ea20d055d0917020fde4e1cd290c6b3c15929809
Anatoliy <anartius@gmail.com>
2022-03-11 18:03:32 +0100

    Stage 1/5 is completed.

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -1,5 +1,34 @@
 package chess
 
+import kotlin.math.abs
+
 fun main() {
-//    write your code here
+    val field = mutableListOf<MutableList<String>>()
+    println("Pawns-Only Chess")
+    createField(field)
+    printField(field)
+}
+
+fun createField(field: MutableList<MutableList<String>>) {
+    val gaps = MutableList(8) { " " }
+    field.add(gaps)
+    field.add(MutableList(8) { "B" })
+    for (i in 0..3) {
+        field.add(gaps)
+    }
+    field.add(MutableList(8) { "W" })
+    field.add(gaps)
+}
+
+fun printField(field: MutableList<MutableList<String>>) {
+    val line = "  " + "+---".repeat(8) + "+"
+    val labels = ('a'..'h').toMutableList()
+    for (i in field.indices) {
+        println(line)
+        println(
+        field[i].joinToString(prefix = "${abs(i - 8)} | ", separator = " | ", postfix = " |")
+        )
+    }
+    println(line)
+    println(labels.joinToString(prefix = "    ", separator = "   "))
 }
